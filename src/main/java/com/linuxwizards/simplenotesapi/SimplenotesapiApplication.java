b9270e6ff0f08b4d8ffce03b1f92415d56555ec3
DarkoGNU <42816979+DarkoGNU@users.noreply.github.com>
2024-02-19 21:30:28 +0100

    Spring Initializr template
    Co-authored-by: imthernet <39921871+imthernet@users.noreply.github.com>

---
+++ src/main/java/com/linuxwizards/simplenotesapi/SimplenotesapiApplication.java
@@ -0,0 +1,13 @@
+package com.linuxwizards.simplenotesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplenotesapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplenotesapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/linuxwizards/simplenotesapi/SimplenotesapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.linuxwizards.simplenotesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplenotesapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
