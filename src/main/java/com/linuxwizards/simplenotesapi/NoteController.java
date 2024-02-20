42594a360f39c2cde3a7599aba86b5cacc616af9
DarkoGNU <42816979+DarkoGNU@users.noreply.github.com>
2024-02-20 17:35:38 +0100

    Implement POST
    Co-authored-by: imthernet <39921871+imthernet@users.noreply.github.com>

--- src/main/java/com/linuxwizards/simplenotesapi/NoteController.java
+++ src/main/java/com/linuxwizards/simplenotesapi/NoteController.java
@@ -2,11 +2,15 @@ package com.linuxwizards.simplenotesapi;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.Optional;
+import java.net.URI;
 
 @RestController
 @RequestMapping("/notes")
@@ -27,5 +31,16 @@ class NoteController {
         } else {
             return ResponseEntity.notFound().build();
         }
+    }
+
+    @PostMapping
+    private ResponseEntity<Void> createNote(@RequestBody Note newNoteRequest, UriComponentsBuilder ucb) {
+        Note savedNote = noteRepository.save(newNoteRequest);
+        URI locationOfNewNote = ucb
+                .path("notes/{id}")
+                .buildAndExpand(savedNote.id())
+                .toUri();
+
+        return ResponseEntity.created(locationOfNewNote).build();
     }
 }
--- src/test/java/com/linuxwizards/simplenotesapi/SimplenotesapiApplicationTests.java
+++ src/test/java/com/linuxwizards/simplenotesapi/SimplenotesapiApplicationTests.java
@@ -8,6 +8,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import java.net.URI;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -40,5 +41,25 @@ class SimplenotesapiApplicationTests {
 
 		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
 		assertThat(response.getBody()).isBlank();
+	}
+
+	@Test
+	void shouldCreateANewNote() {
+		Note newNote = new Note(null, "This is second note", "This is second content");
+		ResponseEntity<Void> createResponse = restTemplate.postForEntity("/notes", newNote, Void.class);
+		assertThat(createResponse.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+
+		URI locationOfNewNote = createResponse.getHeaders().getLocation();
+		ResponseEntity<String> getResponse = restTemplate.getForEntity(locationOfNewNote, String.class);
+		assertThat(getResponse.getStatusCode()).isEqualTo(HttpStatus.OK);
+
+		DocumentContext documentContext = JsonPath.parse(getResponse.getBody());
+		Number id = documentContext.read("$.id");
+		String title = documentContext.read("$.title");
+		String content = documentContext.read("$.content");
+
+		assertThat(id).isNotNull();
+		assertThat(title).isEqualTo("This is second note");
+		assertThat(content).isEqualTo("This is second content");
 	}
 }
