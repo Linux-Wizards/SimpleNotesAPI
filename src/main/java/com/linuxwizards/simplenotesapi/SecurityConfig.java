0eb8b194a08ec9552e059b9d2069308e60852144
DarkoGNU <42816979+DarkoGNU@users.noreply.github.com>
2024-02-21 18:47:22 +0100

    Configure CORS instead of disabling it
    Co-authored-by: imthernet <39921871+imthernet@users.noreply.github.com>

--- src/main/java/com/linuxwizards/simplenotesapi/SecurityConfig.java
+++ src/main/java/com/linuxwizards/simplenotesapi/SecurityConfig.java
@@ -11,20 +11,36 @@ import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 class SecurityConfig {
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
+                .cors(httpSecurityCorsConfigurer -> httpSecurityCorsConfigurer.configurationSource(corsConfigurationSource()))
                 .authorizeHttpRequests(request -> request
                         .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
                         .requestMatchers("/notes/**")
                         .hasRole("NOTES-USER"))
                 .httpBasic(Customizer.withDefaults())
-                .csrf(csrf -> csrf.disable())
-                .cors(cors -> cors.disable());
+                .csrf(csrf -> csrf.disable());
         return http.build();
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("http://localhost:3000"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 
     @Bean
